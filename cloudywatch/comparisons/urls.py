c84f6a56b9e2357f7bbe89a60ad54cddd800cf46
A.Sichkar <a.sichkar@code-on.be>
2013-11-08 13:35:39 +0200

    #10 comparisons ui

--- cloudywatch/comparisons/urls.py
+++ cloudywatch/comparisons/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('comparisons.views',
+    url(r'^$', 'detail', name='comparison_detail'),
     url(r'^([\w-]+)/$', 'detail', name='comparison_detail'),
 )
--- cloudywatch/comparisons/views.py
+++ cloudywatch/comparisons/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Comparison
 from utils.views import render_to
 
 
 @render_to('comparitions/detail.html')
-def detail(request, comparison_slug):
-    comparison = get_object_or_404(Comparison, slug=comparison_slug)
+def detail(request, comparison_slug=None):
+    print comparison_slug
+    comparisons = Comparison.objects.all()
+    if comparison_slug:
+        comparison = get_object_or_404(Comparison, slug=comparison_slug)
+    else:
+        try:
+            print comparisons[0].get_absolute_url()
+            return redirect(comparisons[0])
+        except IndexError:
+            raise Http404
+
     return {
         'comparison': comparison,
+        'comparisons': comparisons
     }
